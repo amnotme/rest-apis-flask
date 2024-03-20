8c289532613df0b14343526ada965353b4eefd3b
amnotme <leopoldo.hernandez.oliva@gmail.com>
2024-03-20 15:22:24 -0500

    adding settings.py

--- tasks.py
+++ tasks.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from dotenv import load_dotenv
+import jinja2
 
 load_dotenv()
 
@@ -8,7 +9,13 @@ api_key = os.getenv("MAILGUN_API_KEY")
 domain_name = os.getenv("MAILGUN_DOMAIN")
 email = os.getenv("MAIL_GUN_EMAIL")
 
-def send_simple_message(to, subject, body):
+template_loader = jinja2.FileSystemLoader("templates")
+template_env = jinja2.Environment(loader=template_loader)
+
+def render_template(template_filename, **context):
+    return template_env.get_template(template_filename).render(**context)
+
+def send_simple_message(to, subject, body, html):
     return requests.post(
         f"https://api.mailgun.net/v3/{domain_name}/messages",
         auth=("api", api_key),
@@ -17,11 +24,15 @@ def send_simple_message(to, subject, body):
             "to": [to],
             "subject": subject,
             "text": body,
+            "html": html
         },
     )
 
 
 def send_user_registration_email(username):
     return send_simple_message(
-        email, "Successfully signed up", f"Hi, Someone signed up! {username} successfully signed up."
+        to=email,
+        subject="Successfully signed up",
+        body=f"Hi, Someone signed up! {username} successfully signed up.",
+        html=render_template("email/action.html", username=username)
     )
